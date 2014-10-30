353174f66ba9c3cbc2a3270ecb2fea7d8f1c138b
HsRise <Hswbinforgithub@gmail.com>
2014-10-30 10:45:09 +0800

    a new private repostiorie

--- app/src/main/java/example/com/textproject1/ExpandableListViewActivity.java
+++
@@ -1,9 +0,0 @@
-package example.com.textproject1;
-
-import android.content.Context;
-import android.util.AttributeSet;
-import android.widget.ExpandableListView;
-
-public class ExpandableListViewAcitivity extends ExpandableListView{
-
-}
